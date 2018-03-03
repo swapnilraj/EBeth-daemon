91738146df9070b5a34c9f00be6108e44ca4c225
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-03 17:33:23 +0000

    Implement API calls for fixtures

---
+++ src/league-api.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://fplalerts.com/api/fpl_lhs_17.php';
+
+export const getFixtures = async () : Promise<any> => {
+  const requestOpt = {
+    method: 'GET',
+    url: BASE_URL,
+  };
+
+  return axios(requestOpt)
+    .then(res => res.data.scores)
+    .catch(err => err);
+}
