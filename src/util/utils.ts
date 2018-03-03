e11fd15b579604e1cbf0c90127fd882d439be8d7
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-03 21:54:05 +0000

    Add toScheduleFormat utility function * laterjs has really bad documentation

---
+++ src/util/utils.ts
@@ -0,0 +1,12 @@
+export const toScheduleFormat = (date: Date) => {
+  return {
+    schedules: [{
+      h: [ date.getHours() ],
+      m: [ date.getMinutes() ],
+      s: [ date.getSeconds() ],
+      D: [ date.getDate() ],
+      M: [ date.getMonth() + 1 ],
+      Y: [ date.getFullYear() ],
+    }]
+  };
+}
