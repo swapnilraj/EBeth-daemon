675f39d8dd3cb81bd7bea29171359fb6d355ef4c
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-03 21:55:07 +0000

    Add deploy and startMatch placeholders * The timers are configured

--- src/util/ebeth-contract.ts
+++ src/util/ebeth-contract.ts
@@ -1,10 +1,19 @@
+import * as later from 'later';
+import {toScheduleFormat} from './utils';
 let alreadyDeployed = {};
 
 export const deploy = (fixture) => {
   if(!(fixture.fid in alreadyDeployed)) {
     alreadyDeployed = {...alreadyDeployed, [fixture.fid]: true};
-    console.log(fixture)
+    console.log(fixture);
+
+    const startTime = toScheduleFormat(new Date(fixture['ko_f']));
+    later.setTimeout(() => { startMatch(fixture.fid)}, startTime);
   }
   // Currently only logs the fixture later it will deploy the contract
   // if not already deployed
-}
+};
+
+export const startMatch = (fid) => {
+  console.log(fid);
+};
