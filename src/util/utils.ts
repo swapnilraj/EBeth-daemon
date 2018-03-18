d7e3c85855c157d71ba81a6bda083845398e37d4
Swapnil Raj <raj.swapnil99@gmail.com>
2018-03-18 19:21:33 +0000

    Minor refactor

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,18 +1,16 @@
 import { MILLISECONDS_IN_MINUTES } from './constants';
 
-export const toScheduleFormat = (date: Date) => {
-  return {
-    schedules: [
-      {
-        h: [date.getHours()],
-        m: [date.getMinutes()],
-        s: [date.getSeconds()],
-        D: [date.getDate()],
-        M: [date.getMonth() + 1],
-        Y: [date.getFullYear()],
-      },
-    ],
-  };
-};
+export const toScheduleFormat = (date: Date) => ({
+  schedules: [
+    {
+      h: [date.getHours()],
+      m: [date.getMinutes()],
+      s: [date.getSeconds()],
+      D: [date.getDate()],
+      M: [date.getMonth() + 1],
+      Y: [date.getFullYear()],
+    },
+  ],
+});
 
 export const minutesToMilliSeconds = (minutes: number) => minutes * MILLISECONDS_IN_MINUTES;
