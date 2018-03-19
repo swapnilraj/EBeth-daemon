79f5096afad05c261b269f5e23044be31f0c2232
Ebin-Benny <bennye@tcd.ie>
2018-03-19 15:14:50 +0000

    Added isFinished method to check if a game is finished.

--- src/league-api.ts
+++ src/league-api.ts
@@ -2,6 +2,10 @@ import axios from 'axios';
 
 const BASE_URL = 'http://fplalerts.com/api/fpl_lhs_17.php';
 
+/**
+ * Returns an array of upcoming premier league matches.
+ * @returns Array of upcoming premier league matches.
+ */
 export const getFixtures = async (): Promise<any> => {
   const requestOpt = {
     method: 'GET',
@@ -10,5 +14,21 @@ export const getFixtures = async (): Promise<any> => {
 
   return axios(requestOpt)
     .then(res => res.data.nextgw)
+    .catch(err => err);
+};
+
+/**
+ * Returns a boolean signifying if the match is over or not.
+ * @param index The index of the game in the api.
+ * @returns Boolean signifying if the match is over or not.
+ */
+export const isFinished = async (index: number): Promise<boolean> => {
+  const requestOpt = {
+    method: 'GET',
+    url: BASE_URL,
+  };
+
+  return axios(requestOpt)
+    .then(res => res.data.scores[index].fin === '1')
     .catch(err => err);
 };
