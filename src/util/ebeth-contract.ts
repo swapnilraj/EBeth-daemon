6670bf8272740d5449b3b36032167da472610633
Ebin-Benny <bennye@tcd.ie>
2018-03-19 17:31:01 +0000

    Added a timer that checks if a match is finished before calling stopMatch()

--- src/util/ebeth-contract.ts
+++ src/util/ebeth-contract.ts
@@ -1,6 +1,7 @@
 import * as later from 'later';
 import { toScheduleFormat, minutesToMilliSeconds } from './utils';
 import { CONTRACT_LOCATION } from './constants';
+import { isFinished } from '../league-api';
 const Web3 = require('web3');
 
 const web3 = new Web3();
@@ -8,16 +9,23 @@ web3.setProvider(new Web3.providers.HttpProvider('http://0.0.0.0:8545'));
 
 const bettingContractJSON = require(CONTRACT_LOCATION + '/Betting.json');
 const managerContractJSON = require(CONTRACT_LOCATION + '/BetManager.json');
-const managerAddress = '0x72212ec4a000c3f7667661cf6b873d7e15e496f2';
 const bettingContract = new web3.eth.Contract(bettingContractJSON['abi']);
 const managerContract = new web3.eth.Contract(managerContractJSON['abi']);
+
+const managerAddress = '0xdbc746bfec04d2641a6edeef5233eb9628303b39';
 
 bettingContract.options.data = bettingContractJSON['bytecode'];
 managerContract.options.address = managerAddress;
 
 let alreadyDeployed = {};
 
-export const deploy = async (fixture, index) => {
+/**
+ * Deploys a fixture as a smart contract and starts timers to manage the state of
+ * the contract.
+ * @param fixture An object containing information about the fixture.
+ * @param index The index of the fixture in the api.
+ */
+export const deploy = async (fixture: any, index: number) => {
   if (!(fixture.fid in alreadyDeployed)) {
     try {
       alreadyDeployed = { ...alreadyDeployed, [fixture.fid]: true };
@@ -37,6 +45,25 @@ export const deploy = async (fixture, index) => {
 
       fixture['address'] = newContractInstance.options.address;
       await sendToManager(fixture, accounts, startTime);
+
+      later.setTimeout(() => {
+        startMatch(fixture);
+      }, toScheduleFormat(startTime));
+
+      const stopTime = new Date(startTime.getTime() + minutesToMilliSeconds(105));
+
+      const sch = later.parse.recur.every(2).minute();
+
+      later.setTimeout(() => {
+        let t = later.setInterval(() => {
+          isFinished(index).then(res => {
+            if (res === true) {
+              stopMatch(fixture);
+              t.clear();
+            }
+          });
+        }, sch);
+      }, toScheduleFormat(stopTime));
     } catch (e) {
       delete alreadyDeployed[fixture.fid];
       console.log('Deploying contract was cancelled due to error: ' + e + '\n Trying again.');
@@ -45,30 +72,27 @@ export const deploy = async (fixture, index) => {
   }
 };
 
+/**
+ * Send the address of the Betting smart contract to the BetManager smart contract.
+ * @param fixture An object containing information about the fixture.
+ * @param accounts An array of Ethereum accounts given by web3.
+ * @param startTime The Date object for when the match is meant to start.
+ */
 const sendToManager = async (fixture, accounts, startTime) => {
   try {
-    console.log('Sending to manager ' + fixture.fid + ':' + fixture['tm_h'] + ' vs ' + fixture['tm_a']);
     const receipt = await managerContract.methods.addEvent(fixture.address).send({ from: accounts[0] });
-
     console.log(`Created match id: ${fixture.fid} Tx: ${receipt.transactionHash}`);
-
-    later.setTimeout(() => {
-      startMatch(fixture);
-    }, toScheduleFormat(startTime));
-
-    const stopTime = new Date(startTime.getTime() + minutesToMilliSeconds(130));
-    later.setTimeout(() => {
-      stopMatch(fixture.fid);
-    }, toScheduleFormat(stopTime));
-
-    console.log('Fixture ' + fixture.fid + ' has been sent to manager.');
   } catch (e) {
     console.log('Sending info for ' + fixture.fid + 'to manager was cancelled due to error: ' + e + '\n Trying again.');
     await sendToManager(fixture, accounts, startTime);
   }
 };
 
-export const startMatch = async fixture => {
+/**
+ * Signals the smart contract to stop accepting bets.
+ * @param fixture An object containing information about the fixture.
+ */
+const startMatch = async fixture => {
   console.log(`Starting match id: ${fixture.fid}`);
   bettingContract.options.address = fixture.address;
   const accounts = await web3.eth.getAccounts();
@@ -81,16 +105,20 @@ export const startMatch = async fixture => {
     .catch(function(error) {
       console.log(error);
     });
-  console.log(fixture.fid);
+  console.log('Match started: ' + fixture.fid);
 };
 
-export const stopMatch = async fixture => {
+/**
+ * Signals the smart contract that the match is over.
+ * @param fixture An object containing information about the fixture.
+ */
+const stopMatch = async fixture => {
   console.log(`Deleting ${fixture.fid} from alreadyDeployed`);
   bettingContract.options.address = fixture.address;
   const accounts = await web3.eth.getAccounts();
   bettingContract.methods
     .eventOver()
-    .send({ from: accounts[0], value: 6000000000000000, gas: 200000, gasPrice: '300000000000' })
+    .send({ from: accounts[0], value: 12000000000000000, gas: 200000, gasPrice: '300000000000' })
     .then(function(receipt) {
       console.log(receipt.transactionHash);
     })
