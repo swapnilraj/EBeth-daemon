24417c8a67970813bd67d5dd27ab051736f7842a
Ebin-Benny <bennye@tcd.ie>
2018-03-21 11:32:08 +0000

    Added comment for documentation.

--- src/daemon.ts
+++ src/daemon.ts
@@ -3,8 +3,10 @@ import { deploy } from './util/ebeth-contract';
 
 let currentFixtures = [];
 
+/**
+ * Gets fixtures from the api and deploys them.
+ */
 export const daemon = async () => {
   currentFixtures = await getFixtures().then(r => r);
-
   currentFixtures.forEach(deploy);
 };
